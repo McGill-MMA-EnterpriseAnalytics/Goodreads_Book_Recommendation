e26784c026f89b17b0f6cb5f8c02a251aa0becab
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2023-02-05 20:39:40 -0500

    downloaded read history

--- Data/Goodreads_users+books_RM.py
+++ Data/Goodreads_users+books_RM.py
@@ -1,6 +1,8 @@
+#%%
+# 
 # Asynchronous
-import nest_asyncio
-nest_asyncio.apply()
+# import nest_asyncio
+# nest_asyncio.apply()
 import asyncio
 import aiohttp
 import json
@@ -16,8 +18,9 @@ import numpy as np
 import json
 
 import re
-cookies = {
-    'ccsid': '027-9406763-1350835',
+import os
+
+cookies = {'ccsid': '027-9406763-1350835',
     '__qca': 'P0-867993267-1671835417644',
     'p': 'BFnM5Q76wF0shLrczPEWD6Ai6glvalBOI0HQnM7wAheYffUa',
     'likely_has_account': 'true',
@@ -35,11 +38,8 @@ cookies = {
     'x-main': '"QKUGsTWzgOIn1MQ?MLW6M7?W9sXe6vT8lOfYIRdLo30O9fDyRRuxqM6Up6QliSso"',
     'at-main': 'Atza|IwEBIE9-j0Q56LUPug78YuRWyKfqUI-71kqBUKBSv9o0pgXoEdWRTp3QanY2PVmzS5kuRKhJ-2Qf2Y2Xza8aOzNTehTteyzG_d1HL64CDYGBvcFiM3J3K6q-_iEKTHPqIuFGTC7GBQffrkFancRYWCUTNK7ii9qpL1Qvc-lEAmj5xJFsos2I98msXxsTvgVFMUfr1rhJK6CfvkpCAIziz2lPcltlQCWaFsR10_Le_ytyKVYeIZ18m-F1bXhhejn7mTwQJl0',
     'sess-at-main': '"f/c/JR/xicYEmsJ7sGnJySc4uAQ6ojXuaOFFChXMGho="',
-    '_session_id2': '64c06a0ae25fee31093af8a3bf3f79e1',
-}
-
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    '_session_id2': '64c06a0ae25fee31093af8a3bf3f79e1',}
+headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,/;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'Accept-Language': 'en-US,en;q=0.9',
     'Cache-Control': 'max-age=0',
     'Connection': 'keep-alive',
@@ -54,15 +54,15 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
     'sec-ch-ua': '"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
     'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"macOS"',
-}
+    'sec-ch-ua-platform': '"macOS"',}
+params = {'ref': 'nav_comm_people',}
 
-params = {
-    'ref': 'nav_comm_people',
-}
 
-f = open('Data/reviews_urls.json')
-review_urls = json.load(f)
+directory = os.path.realpath(os.path.join(os.getcwd()))
+os.chdir(directory)
+
+with open(os.path.join(directory, 'Data','reviews_urls.json')) as f:
+    review_urls = json.load(f)
 
 # THIS MODULE ITERATES OVER ALL USER DATA
 # each page contains a list of 20 books that user has read
@@ -103,6 +103,7 @@ async def get_read_books(urls_to_view):
     async with aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=60*60*24)) as session:
 
         tasks = []
+        print(tasks)
         for rev_url, user in urls_to_view:
             tasks.append(asyncio.ensure_future(get_user_read_books(session, rev_url, user)))
 
@@ -116,7 +117,11 @@ async def get_read_books(urls_to_view):
         return(failed_urls_return)
 
 
+#%%
 read_history = []
 while len(review_urls) > 0:
     review_urls = asyncio.run(get_read_books(review_urls))
     print(review_urls)
+
+with open('read_history.json', 'r') as file:
+    json.dump(read_history, file, indent=2)
