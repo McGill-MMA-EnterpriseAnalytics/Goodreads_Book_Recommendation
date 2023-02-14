097d39c868c3c7372a64745ac4ee7590b28bb819
ruhimahendra24 <ruhimahendra24@gmail.com>
2023-02-14 16:10:48 -0500

    Konstanin's Stuffs

--- Data/functions.py
+++ Data/functions.py
@@ -1,15 +1,17 @@
 import aiohttp
 import asyncio
 import tqdm.asyncio
-import BeautifulSoup
+from bs4 import BeautifulSoup
+import re
+import numpy as np
 
 
-async def async_downloader(downloader: function, task_list: list) -> tuple(list, list):
+async def async_downloader(parser_function: function,  task_list: list) -> tuple(list, list):
     '''This is a generic asyncronous request function for downloading data
 
     Inputs:
-        (1) Downloader Function - this function should return whether it was
-            successful or not and the corresponding data
+        (1) Downloader Function - this function should take page soup and return 
+            relevant data
         (2) Task List - each item in the list should be enough to execute
             the downloader function
 
@@ -23,7 +25,7 @@ async def async_downloader(downloader: function, task_list: list) -> tuple(list,
         # Initializes tasks
         async_tasks = []
         for task in task_list:
-            async_tasks.append(asyncio.ensure_future(downloader(session, task)))
+            async_tasks.append(asyncio.ensure_future(page_downloader(session, task, parser_function)))
 
         # Executes Tasks
         task_res = await tqdm.asyncio.tqdm_asyncio.gather(*async_tasks)
@@ -43,8 +45,8 @@ async def async_downloader(downloader: function, task_list: list) -> tuple(list,
         return (successful_task_data, failed_task_data)
 
 
-# GET USER LIST
-async def get_user_list(session, task_data):
+
+async def page_downloader(session, task_data, parser_function):
     '''Downloads a list of user
 
     Inputs:
@@ -57,22 +59,51 @@ async def get_user_list(session, task_data):
                 - if sucessful returns list of data points
                 - if not sucessful returns failed task data '''
     try:
-        url = task_data[0] + task_data[1] + task_data[2]
+        url = task_data[0]
         async with session.get(url, params=params, cookies=cookies, headers=headers) as response:
-            # Request
             page = await response.text()
             soup = BeautifulSoup(page, 'html.parser')
-
-            # Parse
-            tab = soup.find("table", {"class": "tableList"})
-            results = []
-            trs = tab.find_all('tr')
-            for t in trs:
-                s = t.find_all('td')[1].find('a', href=True)['href']
-                user_id = re.findall(r'\d+', s)[0]
-                results.append((user_id, task_data[1]))
-
-            # Results
+            results = parser_function(soup, task_data)
             return (True, results)
     except Exception as e:
         return (False, task_data)
+
+
+# GET USER IDS AND COUNTRIES
+def get_user_list(soup, task_data):
+    tab = soup.find("table", {"class": "tableList"})
+    results = []
+    trs = tab.find_all('tr')
+    for t in trs:
+        s = t.find_all('td')[1].find('a', href=True)['href']
+        user_id = re.findall(r'\d+', s)[0]
+        results.append((user_id, task_data[1]))
+        
+    return(results)
+    
+
+def get_user_detail(soup, task_data):
+    details = soup.find("div", class_="infoBoxRowItem").text.split(',')
+    age = ""
+    gender = ""
+
+    for i in details:
+        if 'Age ' in i:
+            age = re.findall(r'\d+', i)[0]
+        if 'Female' in i or 'Male' in i:
+            gender = i.strip()
+    
+    results = [task_data[1], age, gender]
+    
+
+def get_page_details(soup, task_data): 
+    review_pages = int(soup.find(id='reviewPagination').find_all("a")[-2].text)
+    if review_pages > 5:
+        check_pages = np.random.choice(review_pages, replace=False, size=5)
+    else: 
+        check_pages = [i for i in range(review_pages)]
+
+    new_urls = []
+    for p in check_pages:
+                new_urls.append((f"https://www.goodreads.com/review/list/{task}?page={p+1}",task))
+            results.extend(new_urls)
