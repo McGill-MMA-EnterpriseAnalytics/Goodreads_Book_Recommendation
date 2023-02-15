7711112908a2c72d11124cfed7895b36f2bc083a
Tyler Nagano <56421574+tnagano99@users.noreply.github.com>
2023-02-15 18:11:03 -0500

    minor changes

--- Modelling/Book_clustering.py
+++ Modelling/Book_clustering.py
@@ -2,7 +2,6 @@
 import plotly.express as px
 from yellowbrick.cluster.icdm import InterclusterDistance
 from yellowbrick.cluster import KElbowVisualizer
-from sklearn.impute import IterativeImputer
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +12,7 @@ import os
 
 from sklearn.decomposition import PCA
 from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 from sklearn.model_selection import GridSearchCV
 from sklearn.neural_network import MLPRegressor
 from sklearn.preprocessing import OneHotEncoder, StandardScaler, PowerTransformer, MinMaxScaler
@@ -67,7 +67,7 @@ df['Book_Counts'] = df['Book_Counts'].replace({np.nan: 0})
 
 # remove unique identifiers/unnecessary column
 df = df.dropna(how="any")
-df_store = df[["isbn", "book_id", "genre", "author", "title"]]
+df_store = df[["isbn", "book_id", "genre", "author", "title"]] # store these for recommendation system
 df = df.drop(columns=["url", "isbn", "book_id", "title"])
 
 # split punlish into year month day columns from string
@@ -162,45 +162,53 @@ df_prep_out = df_prep.drop(columns='book_counts')
 # %% ### 3) Model Testing
 
 # #### KMeans
-inertia = []
-for i in range(5, 20):
-    km = KMeans(n_clusters=i, n_init=10, random_state=42)
-    model = km.fit(df_prep_out)
-    print(model.inertia_)
-    inertia.append(model.inertia_)
-    labels = model.labels_
+# inertia = []
+# for i in range(5, 20):
+#     km = KMeans(n_clusters=i, n_init=10, random_state=42)
+#     model = km.fit(df_prep_out)
+#     print(model.inertia_)
+#     inertia.append(model.inertia_)
+#     labels = model.labels_
 
+# visualize the KMeans data to determine number of cluster centers
 km = KMeans(n_init=10, random_state=42)
 vis = KElbowVisualizer(km, k=(2, 30))
 
 vis.fit(df_prep_out)
 vis.show()
 
+# plot the first two principal components
 km = KMeans(n_clusters=9, n_init=10, random_state=42)
 vis_clust = InterclusterDistance(km)
 vis_clust.fit(df_prep_out)
 vis_clust.show()
 
+# visualize the principal components in 2D and 3D
 pca = PCA(n_components=df_prep_out.shape[1]-1)
 X_PCA = pca.fit_transform(df_prep_out)
 df_PCA = pd.DataFrame(X_PCA, columns=[f"PC{i}" for i in range(1, len(df_prep_out.columns))])
 
+# calculate the components
 df_components = pd.DataFrame(pca.components_, columns=df_prep_out.columns)
 df_components = df_components.T
 df_components.columns = [f"PC{i}" for i in range(1, len(df_prep_out.columns))]
 
+# check metrics
 pca.explained_variance_ratio_.round(2)
 df_PCA.head()
 df_components.round(2)
 
+# transform data by PCA
 fitted = km.fit(df_prep_out)
 labels = fitted.labels_
 centers = fitted.cluster_centers_
 
+# get centers of clusters in a dataframe for plotting
 pca_centers = pca.transform(centers)
 df_centers = pd.DataFrame(pca_centers, columns=[f"PC{i}" for i in range(1, len(df_prep_out.columns))])
 df_centers
 
+# plot PC1 vs PC2; update pc1 and pc2 to see whichever PCs you want
 pc1 = 1
 pc2 = 2
 pcs = [f"PC{i}" for i in range(1, len(df_prep_out.columns))]
@@ -211,6 +219,7 @@ plt.title(f"{pcs[pc1-1]} by {pcs[pc2-1]}")
 plt.xlim((max(df_PCA[pcs[pc1-1]].min(), -10) - 0.2, min(df_PCA[pcs[pc1-1]].max(), 10) + 0.2))
 plt.ylim((max(df_PCA[pcs[pc2-1]].min(), -10) - 0.2, min(df_PCA[pcs[pc2-1]].max(), 10) + 0.2))
 
+# plot 3d PC1 vs PC2 vs PC3; update pc1, pc2, and pc3 to see whichever PCs you want
 pc1 = 1
 pc2 = 2
 pc3 = 3
@@ -227,7 +236,7 @@ ax.set_zlabel(f"PC{pc3}")
 
 plt.show()
 
-
+# Visualize the data in 3d using plotly for interative plots
 pc1 = 1
 pc2 = 2
 pc3 = 3
@@ -239,69 +248,104 @@ fig.show(renderer="browser")
 
 
 # %% ### 4) For each user get top clusters to recommend
+
+# determine the books read by each user and raw book counts
 df_users = pd.read_csv("../Data/Data_Final/user_details.csv")
 df_book_users = pd.read_csv("../Data/Data_Final/bookshelves.csv")
 labels = fitted.labels_
 
+# get raw book counts for the data
 df_books = pd.concat([df_prep_out, df_store["book_id"], pd.Series(labels, name="Label")], axis=1)
 df_books = pd.merge(df_books, book_counts, left_on='book_id', right_index=True)
 df_books['Book_Counts'] = df_books['Book_Counts'].replace({np.nan: 0})
-df_books['Book_Counts'] = df_books['Book_Counts'] + 1
+df_books['Book_Counts'] = np.square(df_books['Book_Counts']) # gives more probability to more read books
+df_books['Book_Counts'] = df_books['Book_Counts'] + 1 # add 1 so that when doing probabilities there is a chance a new book is chosen
 
+# determine the books a user has read
 df_books_tmp = pd.merge(df_books, df_book_users, left_on="book_id", right_on="BOOKID", how="inner")  # no missing books to users
 df_all = pd.merge(df_books_tmp, df_users, on="USERID", how="left")  # some books have not been read by users
-user_list = df_all["USERID"].tolist()
+user_list = df_all["USERID"].unique().tolist()
 df_all = df_all.drop(columns=["BOOKID", "age", "gender", "Country", "Book_Counts"])
 
-user = user_list[1]
-df_test = df_all.loc[df_all['USERID'] == user, :]
-read_book_ids = df_test['book_id'].to_list()
-df_test = df_test.drop(columns=["USERID", "Label", "book_id"])
 
-test_preds = km.predict(df_test)
+# %% 
+# subset the data for a single user to see the recommendations
+user = user_list[0]
+count = 0
+for user in user_list:
+    df_test = df_all.loc[df_all['USERID'] == user, :]
+    # read_book_ids = df_test['book_id'].to_list()
+    # df_test = df_test.drop(columns=["USERID", "Label", "book_id"])
+    X_train, X_test = train_test_split(df_test, test_size = 0.3, random_state=42)
+    read_book_ids = X_train['book_id'].to_list()
+    test_read_book_ids = X_test['book_id'].to_list()
+    X_train = X_train.drop(columns=["USERID", "Label", "book_id"])
+    X_train = X_train.dropna(how="any")
+    X_test = X_test.drop(columns=["USERID", "Label", "book_id"])
+    X_test = X_test.dropna(how="any")
 
-num_topics = {}
-for book in test_preds:
-    if str(book) not in num_topics.keys():
-        num_topics[str(book)] = 1
-    else:
-        num_topics[str(book)] += 1
+    # preds = km.predict(df_test)
+    preds = km.predict(X_train)
+    test_preds = km.predict(X_test)
 
-total = 0
-for key in num_topics.keys():
-    num_topics[key] = round(num_topics[key] / len(test_preds) * 20)
-    total += num_topics[key]
+    num_topics = {}
+    for book in preds:
+        if str(book) not in num_topics.keys():
+            num_topics[str(book)] = 1
+        else:
+            num_topics[str(book)] += 1
 
-if total < 20:
-    max_key = max(num_topics, key=num_topics.get)
-    num_topics[max_key] += 1
-elif total > 20:
-    min_key = max(num_topics, key=num_topics.get)
-    num_topics[min_key] -= 1
-
-recommendations = []
-for key in num_topics.keys():
-    df_books_sub = df_books.loc[df_books["Label"] == int(key), :]
-    df_books_sub = df_books_sub.loc[~(df_books_sub['book_id'].isin(read_book_ids)), :]
-
-    ###################################### finds how many users have read the book and adjusts probability of recommending ########################
     total = 0
-    for num in df_books_sub["Book_Counts"]:
-        total += num
-    df_books_sub['Book_Counts'] = df_books_sub['Book_Counts'] / total
+    for key in num_topics.keys():
+        num_topics[key] = round(num_topics[key] / len(preds) * 30)
+        total += num_topics[key]
 
-    choices = np.random.choice(df_books_sub['book_id'].to_numpy(), num_topics[key], p=df_books_sub['Book_Counts'].to_numpy(), replace=False)
-    ###############################################################################################################################################
+    if total < 30:
+        max_key = max(num_topics, key=num_topics.get)
+        num_topics[max_key] += 1
+    elif total > 30:
+        min_key = max(num_topics, key=num_topics.get)
+        num_topics[min_key] -= 1
 
-    recommendations.extend(list(choices))
+    recommendations = []
+    for key in num_topics.keys():
+        df_books_sub = df_books.loc[df_books["Label"] == int(key), :]
+        df_books_sub = df_books_sub.loc[~(df_books_sub['book_id'].isin(read_book_ids)), :]
 
-idx = df_books[df_books['book_id'].isin(recommendations)].index
+        ###################################### finds how many users have read the book and adjusts probability of recommending ########################
+        total = 0
+        for num in df_books_sub["Book_Counts"]:
+            total += num
+        df_books_sub['Book_Counts'] = df_books_sub['Book_Counts'] / total
 
-book_recs = df_store.loc[idx, :]
-read_books = df_store.loc[df_store['book_id'].isin(read_book_ids)]
-read_books
-book_recs
+        choices = np.random.choice(df_books_sub['book_id'].to_numpy(), num_topics[key], p=df_books_sub['Book_Counts'].to_numpy(), replace=False)
+        recommendations.extend(list(choices))
+        ###################################### give recommendations based on popularity in descending order ####################
 
+        # df_books_sub = df_books_sub.sort_values(by="Book_Counts", ascending=False).reset_index(drop=True)
+
+        # recommendations.extend(df_books_sub.loc[:num_topics[key]-1, "book_id"])
+
+
+    idx = df_books[df_books['book_id'].isin(recommendations)].index
+
+    book_recs = df_store.loc[idx, :]
+    read_books = df_store.loc[df_store['book_id'].isin(read_book_ids)]
+    test_read_books = df_store.loc[df_store['book_id'].isin(test_read_book_ids)]
+
+    read_books
+    book_recs
+    test_read_books
+
+    both = 0
+    for bookA in book_recs['book_id']:
+        for bookB in test_read_books['book_id']:
+            if bookA == bookB:
+                both += 1
+
+    if both > 0:
+        count +=1
+        print(f"Num of overlapping books: {both}")
 
 # %% #### Visualize books
 # transform predicted books
